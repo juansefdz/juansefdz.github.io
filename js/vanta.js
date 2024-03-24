5ae858b6fa82d6120f7bb19b9891b175f65210e8
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-03-24 16:59:53 -0500

    Add files via upload

---
+++ js/vanta.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  VANTA.FOG({
+    el: ".info-container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    highlightColor: 0x0,
+    midtoneColor: 0x5704cf,
+    lowlightColor: 0x5040f,
+    baseColor: 0x50325,
+    blurFactor: 0.51,
+    speed: 2,
+    zoom: 2.40
+
+  });
+});
