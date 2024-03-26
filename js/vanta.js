bc1f88abaed1631094282ef0621c0eb5a749ab6e
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-03-25 21:58:34 -0500

    Add files via upload

--- js/vanta.js
+++ js/vanta.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
     midtoneColor: 0x5704cf,
     lowlightColor: 0x5040f,
     baseColor: 0x50325,
-    blurFactor: 0.51,
-    speed: 2,
-    zoom: 2.4,
+    blurFactor: 0.7,
+    speed: 1,
+    zoom: 2,
   });
 
 });
