40cd78e222ff728860f3dd46f5e355b48a4d76c3
juansefdz <jsfm81091@gmail.com>
2024-12-02 10:50:31 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "juanseFdz.dev",
+    template: "",
+  },
+  description: "Jr. Software Developer",
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import Particles from "./components/particles";
+
+const navigation = [
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
+        <ul className="flex items-center justify-center gap-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="py-3.5 px-0.5 z-10 text-3xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-5xl whitespace-nowrap bg-clip-text ">
+        JUANSEFDZ.dev
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 m-5 ">
+          I'm building{" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam natus deserunt, doloribus qui debitis, tenetur architecto amet dolorem exercitationem repellat distinctio repellendus enim nulla. Repudiandae nulla animi nihil ipsa autem!
+        </h2>
+      </div>
+    </div>
+  );
+
+}
