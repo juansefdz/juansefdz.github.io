0858a677cd8990758cbcc3ad2da25fc57d940527
juansefdz <jsfm81091@gmail.com>
2024-12-16 11:52:19 -0500

    update

--- app/legaltech/page.tsx
+++ app/legaltech/page.tsx
@@ -2,7 +2,6 @@
 
 import Particles from "../components/particles";
 import { Navigation } from "../components/nav";
-import Image from "next/image"; // Si estás usando Next.js
 
 export default function AboutMe() {
   return (
@@ -16,7 +15,6 @@ export default function AboutMe() {
       <div className="max-w-7xl mx-auto px-4 py-20 flex flex-col lg:flex-row items-center justify-center gap-12">
         <div className="flex-1 text-center lg:text-left">
           <h1 className="text-4xl font-semibold text-white mb-4">Legal Tech</h1>
-          
         </div>
       </div>
     </div>
